1caa8a2ac244ccdb4e2900cddc3081df8961cb9c
Nam Luu <123844885+NAMsdrawkcab@users.noreply.github.com>
2024-10-23 13:30:07 -0400

    Add files via upload

---
+++ xkcdDemo/src/test/java/com/classDemo/xkcdDemo/XkcdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.classDemo.xkcdDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XkcdDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
